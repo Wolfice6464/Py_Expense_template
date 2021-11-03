54510e641f7fecb7bda1397458632ef679ca775c
Julien Kherroubi <julien.kherroubi@epita.fr>
2021-11-03 21:53:10 +0100

    feature(expense): expense can now be divided and we can choose who is involved

--- expense.py
+++ expense.py
@@ -1,7 +1,8 @@
 from PyInquirer import prompt
 import csv
+import json
 
-def get_users(answers):
+def get_users(n):
     with open('users.csv', 'r') as f:
         next(f)
         infos = []
@@ -9,6 +10,23 @@ def get_users(answers):
             infos.append(line.rstrip())
 
     return infos
+
+def get_users_json(n):
+    with open('users.csv', 'r') as f:
+        next(f)
+        infos = []
+        chk = n["spender"]
+
+        for line in f:
+            tmp = line.rstrip()
+
+            if tmp == chk :
+                infos.append({'name':tmp,'disabled':'the buyer'})
+            else :
+                infos.append({'name':tmp})
+     
+    return infos
+
 
 expense_questions = [
     {
@@ -27,6 +45,12 @@ expense_questions = [
         "message":"New Expense - Spender: ",
         "choices":get_users,
     },
+    {
+        "type":"checkbox",
+        "name":"involved",
+        "message": "New Expense - Select involved: ",
+        "choices":get_users_json,
+    },
 
 ]
 
@@ -43,5 +67,12 @@ def new_expense(*args):
 
         # write a row to the csv file
         writer.writerow([infos["amount"]] + [infos["label"]] + [infos["spender"]])
+    with open('debt_report.csv', 'a') as f:
+        # create the csv writer
+        writer = csv.writer(f)
+
+        new_price = int(infos["amount"]) / len(infos["involved"])
+        # write a row to the csv file
+        writer.writerow([infos["involved"]] + [new_price] + [infos["spender"]])
     print("Expense Added !")
     return True
