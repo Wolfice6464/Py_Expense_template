964b7f8510e4ce5bcf26f0b98486d0cdda0cd609
Julien Kherroubi <julien.kherroubi@epita.fr>
2021-11-03 19:57:48 +0100

    feature(user, expense, main): add 3 new features

--- expense.py
+++ expense.py
@@ -1,4 +1,5 @@
 from PyInquirer import prompt
+import csv
 
 expense_questions = [
     {
@@ -23,7 +24,15 @@ expense_questions = [
 
 def new_expense(*args):
     infos = prompt(expense_questions)
+    print(infos)
+
     # Writing the informations on external file might be a good idea ¯\_(ツ)_/¯
+    with open('expense_report.csv', 'a') as f:
+        # create the csv writer
+        writer = csv.writer(f)
+
+        # write a row to the csv file
+        writer.writerow([infos["amount"]] + [infos["label"]] + [infos["spender"]])
     print("Expense Added !")
     return True
 
--- user.py
+++ user.py
@@ -1,8 +1,24 @@
 from PyInquirer import prompt
-user_questions = [
+import csv
 
+user_questions = [
+    {
+        "type":"input",
+        "name":"user",
+        "message":"New User: ",
+    },
 ]
 
 def add_user():
+    infos = prompt(user_questions)
+    print(infos)
+
     # This function should create a new user, asking for its name
+    with open('users.csv', 'a') as f:
+        # create the csv writer
+        writer = csv.writer(f)
+
+        # write a row to the csv file
+        writer.writerow([infos["user"]])
+
     return
